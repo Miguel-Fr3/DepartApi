6f1d0c2813310cbb4798ce583e33581a80c04689
Miguel_Fr3 <miguel.fernandes2004f2@gmail.com>
2024-01-16 19:12:23 -0300

    Atualizando classes

---
+++ DepartAPI/Migrations/20240116220444_Case.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DepartApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Case : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Funcionarios_Departamentos_DepartamentoId",
+                table: "Funcionarios");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Funcionarios_DepartamentoId",
+                table: "Funcionarios");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DepartamentosId",
+                table: "Funcionarios",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Funcionarios",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DepartamentosId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Funcionarios",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DepartamentosId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Funcionarios",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "DepartamentosId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Funcionarios",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "DepartamentosId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Funcionarios",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "DepartamentosId",
+                value: null);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Funcionarios_DepartamentosId",
+                table: "Funcionarios",
+                column: "DepartamentosId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Funcionarios_Departamentos_DepartamentosId",
+                table: "Funcionarios",
+                column: "DepartamentosId",
+                principalTable: "Departamentos",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Funcionarios_Departamentos_DepartamentosId",
+                table: "Funcionarios");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Funcionarios_DepartamentosId",
+                table: "Funcionarios");
+
+            migrationBuilder.DropColumn(
+                name: "DepartamentosId",
+                table: "Funcionarios");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Funcionarios_DepartamentoId",
+                table: "Funcionarios",
+                column: "DepartamentoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Funcionarios_Departamentos_DepartamentoId",
+                table: "Funcionarios",
+                column: "DepartamentoId",
+                principalTable: "Departamentos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
